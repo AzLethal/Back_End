45ec2dc6468fd598418d2af7562e65c85b2e548b
Marcelo <Marcelo14axl@gmail.com>
2022-09-09 00:09:43 -0300

    Modulo 3 - Algoritimos

---
+++ JavaScript/Algoritimos/algoritimos/be_js_algoritimo_insertionSort.js
@@ -0,0 +1,15 @@
+const livros = require('./be_js_algoritimos_lista');
+const troca = require('./be_js_troca');
+
+function insertionSort(lista) {
+    for(let atual = 0; atual < lista.length; atual++){
+        let analise = atual;
+        while(analise > 0 && lista[analise].preco < lista[analise -1].preco){
+            troca(lista, analise)
+            analise--;  
+        }
+    }
+    console.log(lista);
+}
+
+insertionSort(livros);
