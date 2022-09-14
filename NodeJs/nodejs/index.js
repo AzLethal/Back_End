8dfad273ca776c7988b0e81ac25aa5cce274237c
Marcelo <Marcelo14axl@gmail.com>
2022-09-14 00:05:30 -0300

    Modulo 1 - NodeJs

---
+++ NodeJs/nodejs/index.js
@@ -0,0 +1,30 @@
+import chalk from 'chalk';
+import fs from 'fs';
+
+const paragrafo = 'Texto retornado por uma função!';
+
+console.log(chalk.blue.bold('Olá Mundo!'));
+// acessado através do node index.js no terminal
+
+
+function texto(string) {
+    return string;
+}
+
+console.log(texto(paragrafo));
+
+//encadear métodos para colorir texto, cor de fundo e texto em negrito
+console.log(chalk.blue.bgWhite.bold('Alura'));
+
+//receber múltiplos argumentos
+console.log(chalk.blue('curso', 'de', 'NodeJS'));
+
+//métodos aninhados
+console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
+
+// uso de template strings e placeholders
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
