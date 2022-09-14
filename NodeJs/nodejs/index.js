4d1cd7aecefde99875547cf04310fe753761130a
Marcelo <Marcelo14axl@gmail.com>
2022-09-14 19:00:52 -0300

    Modulo 3 - NodeJs

--- NodeJs/nodejs/index.js
+++ NodeJs/nodejs/index.js
@@ -5,6 +5,34 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Arquivo inexistente'));
 }
 
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+    while((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({
+            [temp[1]]: temp[2],
+
+        })
+    }
+    return arrayResultados;
+}
+
+//promises with "async e await"
+async function pegaArquivo(path) {
+    const encoding = 'utf-8';
+    try{
+        const texto = await fs.promises.readFile(path, encoding);
+        console.log(extraiLinks(texto));
+    } catch(erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.yellowBright(`Operação Concluída!`));
+    }
+}
+
+pegaArquivo('./arquivos/texto1.md');
+
 //promises with "then"
 /*
 function pegaArquivo(path) {
@@ -15,27 +43,14 @@ function pegaArquivo(path) {
 }
 */
 
-//promises with "async e await"
-async function pegaArquivo(path) {
+/* Sincrono
+function pegaArquivo(path) {
     const encoding = 'utf-8';
-    try{
-        const texto = await fs.promises.readFile(path, encoding);
-        console.log(chalk.green(texto));
-    } catch(erro) {
-        trataErro(erro);
-    } finally {
-        console.log(chalk.yellowBright(`Operação Concluída!`));
-    }
+    fs.readFile(path, encoding, (erro, texto) => {
+        if(erro) {
+            trataErro(erro);
+        }
+        console.log(chalk.greenBright(texto));
+    })
 }
-
-// function pegaArquivo(path) {
-//     const encoding = 'utf-8';
-//     fs.readFile(path, encoding, (erro, texto) => {
-//         if(erro) {
-//             trataErro(erro);
-//         }
-//         console.log(chalk.greenBright(texto));
-//     })
-// }
-
-pegaArquivo('./arquivos/texto1.md');
+*/
